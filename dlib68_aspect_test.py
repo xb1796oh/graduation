2da7db5ee5f952d1c94f3b95cf0bf78f484cab6e
xb1796oh <vhqltkfkd24@gmail.com>
2021-03-10 16:38:08 +0900

    dlib68_aspect_test

---
+++ dlib68_aspect_test.py
@@ -0,0 +1,139 @@
+# python i_dlib68_aspect_test.py -p shape_predictor_68_face_landmarks.dat
+import cv2
+import dlib
+import numpy as np
+import time 
+import argparse
+from imutils import face_utils
+from scipy.spatial import distance as dist
+import matplotlib.pyplot as plt
+
+# 두 세트 사이의 유클리드 거리 계산
+def eye_aspect_ratio(eye):
+	
+	# 수직 landmark (x,y) 거리
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# 수평 eye landmark (x, y) 거리
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+
+	# return the eye aspect ratio
+	return ear
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-v", "--video", type=str, default="",
+	help="path to input video file")
+args = vars(ap.parse_args()) 
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold
+EYE_AR_THRESH = 0.23
+EYE_AR_CONSEC_FRAMES = 4
+
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+TOTAL = 0
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# left, right eye landmark index
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# video stream
+print("[INFO] starting video stream thread...")
+
+cam = cv2.VideoCapture(0) #카메라 생성
+if cam.isOpened() == False: #카메라 생성 확인
+    exit()
+
+#윈도우 생성 및 사이즈 변경
+cv2.namedWindow('CAM_Window')
+
+prev_time = 0 #이전 시간을 저장할 변수
+FPS = 10
+
+earlist = []
+
+while(True):
+
+    ret, frame = cam.read()
+
+    current_time = time.time() - prev_time
+    
+    if(ret is True) and (current_time >1./FPS):
+        prev_time = time.time()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        rects = detector(gray, 0)
+    
+        for rect in rects:
+
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+
+		    # coordinates to compute the eye aspect ratio
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+
+            # average the eye aspect ratio for both eyes
+            ear = (leftEAR + rightEAR) / 2.0
+
+
+            earlist.append(ear)
+
+            # eyehull
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+            if ear < EYE_AR_THRESH:
+                COUNTER +=1
+            
+                if COUNTER >=EYE_AR_CONSEC_FRAMES:
+                    cv2.putText(frame, "Drowsy".format(TOTAL), (300,30),
+			            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+           
+            else:
+                # reset the eye frame counter
+                COUNTER = 0
+
+            cv2.putText(frame, "Counter : {}".format(COUNTER), (10, 30),
+			    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+	
+        #얻어온 이미지 윈도우에 표시
+        cv2.imshow('CAM_Window', frame)
+
+    #10ms 동안 키입력 대기
+    if cv2.waitKey(10) >= 0:
+       break;
+
+cam.release()
+
+# ear 그래프
+plt.plot(earlist)
+plt.show()
+
+
+#윈도우 종료
+cv2.destroyWindow('CAM_Window')
